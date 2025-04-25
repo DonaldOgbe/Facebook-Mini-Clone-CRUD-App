623763c4485a7d4b9034f8db632efcae9eb44543
Donald Ogbe <ogbeebimobowei@gmail.com>
2025-04-25 18:24:33 +0100

    add JDBC and add logic to DatabaseUtil

--- src/main/java/org/deodev/util/DatabaseUtil.java
+++ src/main/java/org/deodev/util/DatabaseUtil.java
@@ -1,4 +1,20 @@
 package org.deodev.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseUtil {
+    private final String URL = "jdbc:postgresql://localhost:5432/facebook_clone";
+    private final String USERNAME = "postgres";
+    private final String PASSWORD = "password";
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("❌ Failed to connect to database: " + e.getMessage());
+            return null;
+        }
+    }
 }
