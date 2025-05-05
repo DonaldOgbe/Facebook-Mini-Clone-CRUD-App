e8d9345c4fc0376bc60aa579a3e0049b253ba907
Donald Ogbe <ogbeebimobowei@gmail.com>
2025-05-05 11:00:48 +0200

    write login endpoint logic in controller and service

--- src/main/java/org/deodev/controller/LoginController.java
+++ src/main/java/org/deodev/controller/LoginController.java
@@ -6,6 +6,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.deodev.dto.request.UserLoginDTO;
+import org.deodev.dto.response.ErrorResponse;
+import org.deodev.model.User;
 import org.deodev.service.AuthService;
 
 import java.io.IOException;
@@ -25,6 +29,30 @@ public class LoginController extends HttpServlet {
 
         ObjectMapper mapper = new ObjectMapper();
 
+        try {
+            UserLoginDTO dto = mapper.readValue(request.getReader(), UserLoginDTO.class);
 
+            User user = authService.login(dto);
+
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user.getEmail());
+
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_OK);
+
+            mapper.writeValue(response.getWriter(), "message: Login Successful");
+
+        } catch (RuntimeException e) {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+            mapper.writeValue(response.getWriter(), new ErrorResponse("Login Failed", e.getMessage()));
+        }  catch (Exception e) {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+            mapper.writeValue(response.getWriter(), new ErrorResponse("Login Failed", e.getMessage()));
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/deodev/service/AuthService.java
+++ src/main/java/org/deodev/service/AuthService.java
@@ -1,36 +1,61 @@
 package org.deodev.service;
 
 import org.deodev.dao.UserDAO;
+import org.deodev.dto.request.UserLoginDTO;
 import org.deodev.dto.request.UserRegistrationDTO;
 import org.deodev.exception.ValidationException;
 import org.deodev.model.User;
+import org.deodev.validation.UserLoginDTOValidator;
 import org.deodev.validation.UserRegistrationDTOValidator;
 import org.deodev.validation.Validator;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class AuthService {
     private final UserDAO userDAO;
-    private Validator<UserRegistrationDTO> dtoValidator = new UserRegistrationDTOValidator();
+    private final Validator<UserRegistrationDTO> userRegistrationDTOValidator = new UserRegistrationDTOValidator();
+    private final Validator<UserLoginDTO> userLoginDTOValidator = new UserLoginDTOValidator();
 
     public AuthService() {
         this.userDAO = new UserDAO();
     }
 
-    public User registerUser(UserRegistrationDTO userRegistrationDto) {
+    public User registerUser(UserRegistrationDTO dto) {
         try {
-            dtoValidator.validate(userRegistrationDto);
+            userRegistrationDTOValidator.validate(dto);
 
-            if (emailExist(userRegistrationDto.getEmail())) {
+            if (emailExist(dto.getEmail())) {
                 throw new ValidationException("Email already exists");
             }
 
-            userRegistrationDto.setPassword(hashPassword(userRegistrationDto.getPassword()));
-            User user = new User(userRegistrationDto);
+            dto.setPassword(hashPassword(dto.getPassword()));
+            User user = new User(dto);
             return userDAO.save(user);
         } catch (ValidationException e) {
             throw e;
         } catch (Exception e) {
-            throw new RuntimeException("User registration failed", e);
+            throw new RuntimeException("Auth Service Error", e);
+        }
+    }
+
+    public User login(UserLoginDTO dto) {
+        try {
+            userLoginDTOValidator.validate(dto);
+
+            if (!emailExist(dto.getEmail())) {
+                throw new ValidationException("Email does not exist");
+            }
+
+            User user = userDAO.findUserByEmail(dto.getEmail());
+
+            if (!BCrypt.checkpw(dto.getPassword(), user.getPassword())) {
+                throw new ValidationException("Incorrect Password");
+            };
+
+            return user;
+        } catch (ValidationException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException("Auth Service Error", e);
         }
     }
 
